29be53d5e5a24b819306c0b6041f53901e57796f
sapinspys <garri009@fiu.edu>
2019-12-06 17:39:46 -0800

    Fixed environment bug

--- twitoff/models.py
+++ twitoff/models.py
@@ -3,11 +3,11 @@ from flask_sqlalchemy import SQLAlchemy
 DB = SQLAlchemy()
 
 # Class name = table name being created
-class User(DB.Model):
+class Users(DB.Model):
     id = DB.Column(DB.Integer, primary_key=True)
     name = DB.Column(DB.String(20), nullable=False)
 
-class Tweet(DB.Model):
+class Tweets(DB.Model):
     id = DB.Column(DB.Integer, primary_key=True)
     # Many SQLALchemy datatypes available
     text = DB.Column(DB.Unicode(280), nullable=False) # Allows for emojis
