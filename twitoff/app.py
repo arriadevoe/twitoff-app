4893f68759a3b268dd61f04a2032d7cb346e4896
sapinspys <garri009@fiu.edu>
2019-12-09 17:18:52 -0800

    Practicing registering user to db

--- twitoff/app.py
+++ twitoff/app.py
@@ -1,11 +1,20 @@
+import uuid
 from flask import Flask, render_template
+from .models import DB, User, Tweet
 
 def create_app():
     app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///my_db.sqlite'
+    DB.init_app(app)
 
     @app.route('/')
     def index():
         # You can add any HTML/CSS to this
+        # We can also add a test user like so:
+        # rand_name = str(uuid.uuid4)
+        # rand_user = User(name=rand_name)
+        # DB.session.add(rand_u)
+        # DB.session.commit()
         return f'Index page'
 
     @app.route('/hello')
