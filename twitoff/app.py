23e838dd0ddc544ac197f3ec51d70877dd5832fb
sapinspys <garri009@fiu.edu>
2019-12-10 22:42:36 -0800

    Installed more dependencies and set up local environment file

--- twitoff/app.py
+++ twitoff/app.py
@@ -1,11 +1,12 @@
+from decouple import config
 from flask import Flask, render_template
 from .models import DB, Users, Tweets
 
 def create_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+    app.config['SQLALCHEMY_DATABASE_URI'] = config('DATABASE_URL') # loads from .env, later a postgres url
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # eliminates warnings
-    app.config['ENV']='debug' # more verbose error handling, don't deploy this!
+    app.config['ENV']=config('ENV')
     DB.init_app(app)
 
     @app.route('/')
