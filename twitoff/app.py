02d34b78b76990b9b4180104ee3105c522cfc320
sapinspys <garri009@fiu.edu>
2019-12-10 16:19:31 -0800

    Simplified app

--- twitoff/app.py
+++ twitoff/app.py
@@ -4,25 +4,21 @@ from .models import DB, Users, Tweets
 
 def create_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///my_db.sqlite'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
     DB.init_app(app)
 
-    @app.route('/')
-    def index():
-        # You can add any HTML/CSS to this
-        # We can also add a test user like so:
-        # rand_name = str(uuid.uuid4)
-        # rand_user = User(name=rand_name)
-        # DB.session.add(rand_u)
-        # DB.session.commit()
-        return f'Index page'
-
-    @app.route('/hello')
-    def hello():
-        return render_template('base.html', title='Hello Page')
+    # You can add any HTML/CSS to this
+    # We can also add a test user like so:
+    # rand_name = str(uuid.uuid4)
+    # rand_user = User(name=rand_name)
+    # DB.session.add(rand_u)
+    # DB.session.commit()
     
-    return app
+    @app.route('/')
+    def root():
+        return render_template('base.html', title='Hello Page')
 
+    return app
 
     # DB.drop_all(), drops all tables
     # DB.create_all(), creates all tables, not needed with the above?
