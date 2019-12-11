7a9765c6457efa67211b8b59bb41a55a26ffa7e3
sapinspys <garri009@fiu.edu>
2019-12-10 20:08:33 -0800

    Cleaned up comments

--- assignment_steps.md
+++ assignment_steps.md
@@ -5,3 +5,15 @@
 4. populate app script with boilerplating
 5. FLASK_APP=twitoff:APP FLASK_ENV=development flask run 
 6. FLASK_APP=twitoff:APP FLASK_ENV=development flask shell
+
+### Steps to manually add users via shell
+* from twitoff.models import *
+* DB.drop_all(), drops all tables
+* DB.create_all(), creates all tables, NEEDED
+* u1 = Users(name = "something")
+* t1 = Tweets(text = "some text")
+* t2 = Tweets(text = "some other text")
+* u1.tweets.append(t1)
+* u1.tweets.append(t2)
+* DB.session.add(u1)
+* DB.session.commit()
--- twitoff/app.py
+++ twitoff/app.py
@@ -4,14 +4,9 @@ from .models import DB, Users, Tweets
 def create_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # eliminates warnings
+    app.config['ENV']='debug' # more verbose error handling, don't deploy this!
     DB.init_app(app)
-
-    # You can add any HTML/CSS to this
-    # We can also add a test user like so:
-    # rand_name = str(uuid.uuid4)
-    # rand_user = User(name=rand_name)
-    # DB.session.add(rand_u)
-    # DB.session.commit()
 
     @app.route('/')
     def root():
@@ -19,14 +14,3 @@ def create_app():
         return render_template('base.html', title='Home', users=users)
 
     return app
-
-    # from twitoff.models import *
-    # DB.drop_all(), drops all tables
-    # DB.create_all(), creates all tables, NEEDED
-    # u1 = Users(name = "something")
-    # t1 = Tweets(text = "some text")
-    # t2 = Tweets(text = "some other text")
-    # u1.tweets.append(t1)
-    # u1.tweets.append(t2)
-    # DB.session.add(u1)
-    # DB.session.commit()
