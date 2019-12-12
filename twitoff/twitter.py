3430c1e71b15583ba9910eef7e242638ed7920a9
sapinspys <garri009@fiu.edu>
2019-12-12 13:41:11 -0800

    Begun implementing Twitter API

---
+++ twitoff/twitter.py
@@ -0,0 +1,11 @@
+"""Retrieve tweets, embeddings, and persists in the database."""
+import tweepy
+from decouple import config
+from .models import DB, Tweet, Users
+
+# Authenticates connection with Twitter
+TWITTER_AUTH = tweepy.OAuthHandler(config('TWITTER_CONSUMER_KEY', config('TWITTER_CONSUMER_SECRET'))
+
+TWITTER_AUTH.set_access_token(config('TWITTER_ACCESS_TOKEN'),config('TWITTER_ACCESS_TOKEN_SECRET'))
+
+TWITTER = tweepy.API(TWITTER_AUTH)
