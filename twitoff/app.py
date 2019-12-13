d6dc6faf1ae7296be68efad1d4f39e4c1a82c4b6
sapinspys <garri009@fiu.edu>
2019-12-12 22:42:25 -0800

    Added new user route

--- twitoff/app.py
+++ twitoff/app.py
@@ -1,6 +1,7 @@
 from decouple import config
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from .models import DB, Users, Tweets
+from .twitter import add_or_update_user
 
 def create_app():
     app = Flask(__name__)
@@ -9,7 +10,7 @@ def create_app():
     app.config['ENV']=config('ENV')
     DB.init_app(app)
 
-    @app.route('/')
+    @app.route('/') # GET by default
     def root():
         users = Users.query.all()
         return render_template('base.html', title='Home', users=users)
@@ -20,5 +21,20 @@ def create_app():
         DB.drop_all()
         DB.create_all()
         return render_template('base.html', title='DB Reset!', users=[])
+
+    @app.route('/user', methods=['POST'])
+    @app.route('/user/<name>', methods=['GET'])
+    def user(name, message):
+        name = name or request.values['user_name']
+        try:
+            if request.method == 'POST':
+                add_or_update_user(name)
+                message = f"User {name} successfully added!"
+            tweets = Users.query.filter(Users.name == name).one().tweets
+        except Exception as e: # very basic, could be more specific plus others
+            message = f"Error adding {name}: {e}"
+            tweets = []
+        return render_template('user.html', title='name', tweets=tweets, message=message)
+
 
     return app
